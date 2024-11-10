9302dd912b11fe033cb8b63397c75d0cd11d5967
Victor Benvides <victorkoto@gmail.com>
2024-11-10 09:03:38 -0300

    wip cache

--- app/src/main/java/com/picpay/desafio/android/data/remote/di/RetrofitModule.kt
+++ app/src/main/java/com/picpay/desafio/android/data/remote/di/RetrofitModule.kt
@@ -1,11 +1,15 @@
 package com.picpay.desafio.android.data.remote.di
 
-
+import android.content.Context
 import com.picpay.desafio.android.data.remote.PicPayService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -20,10 +24,35 @@ object RetrofitModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(baseUrl: String): Retrofit {
+    fun provideCache(@ApplicationContext context: Context): Cache {
+        val cacheSize = 10 * 1024 * 1024L // 10 MB
+        return Cache(context.cacheDir, cacheSize)
+    }
 
+    @Provides
+    @Singleton
+    fun provideCacheInterceptor() = Interceptor { chain ->
+        val request = chain.request().newBuilder()
+            .header("Cache-Control", "public, max-age=300")
+            .build()
+        chain.proceed(request)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(cache: Cache, cacheInterceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(cacheInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String, okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
